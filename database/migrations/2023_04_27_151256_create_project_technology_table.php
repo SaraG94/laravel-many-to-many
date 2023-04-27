923ebf5a598027bdf5c189d5a0574858535ede08
SaraG94 <sara.gherbassi.94@gmail.com>
2023-04-27 17:21:02 +0200

    FEAT migration project_technology

---
+++ database/migrations/2023_04_27_151256_create_project_technology_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_technology', function (Blueprint $table) {
+           //creo la colonna project_id
+            $table->unsignedBigInteger('project_id');
+            //definisco la chiave esterna
+            $table->foreign('project_id')->references('id')->on('projects');
+
+            //creo la colonna technology_id
+            $table->unsignedBigInteger('technology_id');
+            //definisco la chiave esterna
+            $table->foreign('technology_id')->references('id')->on('tecnologies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project_technology');
+        //quando viene eliminata la tabella vengono eliminate le relazioni
+    }
+};
